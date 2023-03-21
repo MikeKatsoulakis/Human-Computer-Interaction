a7516ec97b4770d61de6b92fa18283840a658a25
MikeKatsoulakis <mike_katsoulakis@outlook.com>
2023-03-21 20:32:23 +0200

    Base Project

---
+++ HumanComputerInteraction.DesktopUI/Models/AppConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace HumanComputerInteraction.DesktopUI.Models;
+
+public class AppConfig
+{
+    public string ConfigurationsFolder { get; set; }
+
+    public string AppPropertiesFileName { get; set; }
+}
