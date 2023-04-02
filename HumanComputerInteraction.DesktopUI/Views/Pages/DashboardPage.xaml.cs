69442d70acc6f12f1c0598c79075b893a0c65601
MikeKatsoulakis <mike_katsoulakis@outlook.com>
2023-04-02 20:06:56 +0300

    Examples

--- HumanComputerInteraction.DesktopUI/Views/Pages/DashboardPage.xaml.cs
+++ HumanComputerInteraction.DesktopUI/Views/Pages/DashboardPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Wpf.Ui.Common.Interfaces;
+﻿using HumanComputerInteraction.DesktopUI.ViewModels;
+using System.Windows;
+using Wpf.Ui.Common.Interfaces;
 
 namespace HumanComputerInteraction.DesktopUI.Views.Pages;
 
@@ -14,5 +16,30 @@ public partial class DashboardPage : INavigableView<ViewModels.DashboardViewMode
         ViewModel = viewModel;
 
         InitializeComponent();
+    }
+
+    private void TextBox_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+    {
+        var key = e.Key;
+
+        if (key == System.Windows.Input.Key.Enter)
+        {
+            var contact = new Contact { Name = TextBox1.Text, PhoneNo = TextBox2.Text };
+
+            //23153425325sdfsf34
+
+            if (contact.Name != "" && 
+                contact.PhoneNo != "" && 
+                int.TryParse(contact.PhoneNo, out _))
+            {
+                ViewModel.TestList.Add(contact);
+                TextBox1.Text = "";
+                TextBox2.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Aderfe trollares");
+            }
+        }
     }
 }
